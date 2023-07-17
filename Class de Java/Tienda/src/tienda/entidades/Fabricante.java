1f5bb4b7c4bc6e15258814b1132cda8fa1447ba0
fdbarale <fdbarale@gmail.com>
2023-07-17 02:13:02 -0300

    Revert "Revert "tienda segunda parte""
    This reverts commit 8793dc36e0db135efbaf036cc9a8850fa3f1004c.

---
+++ Class de Java/Tienda/src/tienda/entidades/Fabricante.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tienda.entidades;
+
+/**
+ *
+ * @author pc
+ */
+public class Fabricante {
+    private int codigoFab;
+    private String nombreFab;
+
+    public Fabricante() {
+    }
+
+    public Fabricante(int codigoFab, String nombreFab) {
+        this.codigoFab = codigoFab;
+        this.nombreFab = nombreFab;
+    }
+    
+}
